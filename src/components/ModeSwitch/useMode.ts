b9ee0706ac37602f8bb373aa5b4864e85692c275
Maestro1982 <kris.tote@telenet.be>
2023-08-04 10:02:28 +0200

    create and edit and preview mode

--- src/components/ModeSwitch/useMode.ts
+++ src/components/ModeSwitch/useMode.ts
@@ -1,7 +1,28 @@
-import { useReducer } from "react"
-type Mode = "preview" | "edit"
+import { useEffect, useReducer } from 'react';
+import { useLocation } from 'react-router-dom';
+
+type Mode = 'preview' | 'edit';
 
 export default function useMode() {
-    // TODO: re-implement this hook so that it reads and writes the mode query parameter from the URL
-    return useReducer((mode: Mode) => (mode === "preview" ? "edit" : "preview"), "preview")
+  // TODO: re-implement this hook so that it reads and writes the mode query parameter from the URL
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const modeQueryParam = searchParams.get('mode');
+
+  const initialMode: Mode = modeQueryParam === 'edit' ? 'edit' : 'preview';
+
+  const [mode, toggleMode] = useReducer(
+    (currentMode: Mode) => (currentMode === 'preview' ? 'edit' : 'preview'),
+    initialMode
+  );
+
+  useEffect(() => {
+    // Update the mode query parameter in the URL whenever the mode changes
+    const newSearchParams = new URLSearchParams(location.search);
+    newSearchParams.set('mode', mode);
+    // Replace the current state in the history, without adding a new entry
+    window.history.replaceState({}, '', `?${newSearchParams.toString()}`);
+  }, [mode, location]);
+
+  return [mode, toggleMode] as const;
 }
--- src/components/fields/useFields.ts
+++ src/components/fields/useFields.ts
@@ -1,49 +1,89 @@
-export type Submit = {
-    type: "submit",
-    text: string,
-    id: string
-    label?: never
-    placeholder?: never
-}
-export type Input = {
-    type: "text" | "date" | "number",
-    text?: never,
-    placeholder: string,
-    id: string,
-    label: string | null
-}
-export type Combobox = {
-    type: "country",
-    text?: never,
-    id: string,
-    label: string | null
-    placeholder: string,
-}
-export type Field = Input | Submit | Combobox
+import { useState } from 'react';
 
+export type Submit = {
+  type: 'submit';
+  text: string;
+  id: string;
+  label?: never;
+  placeholder?: never;
+};
+export type Input = {
+  type: 'text' | 'date' | 'number';
+  text?: never;
+  placeholder: string;
+  id: string;
+  label: string | null;
+};
+export type Combobox = {
+  type: 'country';
+  text?: never;
+  id: string;
+  label: string | null;
+  placeholder: string;
+};
+export type Field = Input | Submit | Combobox;
 
 const INIT_FIELDS: Field[] = [
-    { type: "text", placeholder: "Name", label: null, id: self.crypto.randomUUID() },
-    { type: "number", placeholder: "Age", label: null, id: self.crypto.randomUUID() },
-    { type: "date", placeholder: "Date of Birth", label: null, id: self.crypto.randomUUID() },
-    { type: "country", placeholder: "Country", label: null, id: self.crypto.randomUUID() },
-    { type: "submit", text: "submit", id: self.crypto.randomUUID() }
-]
+  {
+    type: 'text',
+    placeholder: 'Name',
+    label: null,
+    id: self.crypto.randomUUID(),
+  },
+  {
+    type: 'number',
+    placeholder: 'Age',
+    label: null,
+    id: self.crypto.randomUUID(),
+  },
+  {
+    type: 'date',
+    placeholder: 'Date of Birth',
+    label: null,
+    id: self.crypto.randomUUID(),
+  },
+  {
+    type: 'country',
+    placeholder: 'Country',
+    label: null,
+    id: self.crypto.randomUUID(),
+  },
+  { type: 'submit', text: 'submit', id: self.crypto.randomUUID() },
+];
 
 /**
  * A store for managing form fields
  * TODO: implement a store for managing form fields
  **/
 
+type FormStore = {
+  fields: Field[];
+  updateField: (id: string, updateField: Field) => void;
+};
+
+export function useFormStore(): FormStore {
+  const [fields, setFields] = useState<Field[]>(INIT_FIELDS);
+
+  const updateField = (id: string, updateField: Field) => {
+    setFields((prevFields) =>
+      prevFields.map((field) => (field.id === 'id' ? updateField : field))
+    );
+  };
+
+  return { fields, updateField };
+}
 
 /**
- * A hook for managing form fields 
+ * A hook for managing form fields
  * TODO: complete this hook by wiring up the form store
- * @returns 
+ * @returns
  */
-function useFields() {
-
-    return { fields: INIT_FIELDS, }
+function useFields(): {
+  fields: Field[];
+  updateField: FormStore['updateField'];
+} {
+  const formStore = useFormStore();
+  return { fields: formStore.fields, updateField: formStore.updateField };
 }
 
 export default useFields;
